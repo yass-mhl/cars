791f9d2a82e6816a2d3db28c9596c3e01731c70c
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-04-28 14:26:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Mycars from './components/Mycars';
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Mycars />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Mycars.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Mycars extends Component{
+  render(){
+    return <h1>Hello react</h1>
+  }
+}
+
+export default Mycars;
