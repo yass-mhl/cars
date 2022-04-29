df88b3d778ecc4e1294540ce3e4ecfa658a031fc
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-04-29 12:24:58 +0200

    Event in React and Change State

--- src/App.js
+++ src/App.js
@@ -8,10 +8,38 @@ class App extends Component {
     title: 'Mon catalogue de voitures'
   }
 
+  changeTitle = (e) => {
+    this.setState({
+      title: "Mon nouveau titre"
+    })
+  }
+
+  changeViaParam = (title) => {
+    this.setState({
+      title
+    })
+  }
+
+  changeViaBind = (title) => {
+    this.setState({
+      title
+    })
+  }
+
+  changeViaInput = (e) => {
+    this.setState({
+     title: e.target.value
+    })
+  }
+
   render () {
     return (
       <div className="App">
         <Mycars title={this.state.title}/>
+        <button onClick={this.changeTitle}>Changer le nom en dur</button>
+        <button onClick={() => this.changeViaParam('Changer le titre via param')}>Changer le nom en dynamique</button>
+        <button onClick={this.changeViaBind.bind(this, 'Titre via bind')}>Titre via bind</button>
+        <input type="text" onChange={this.changeViaInput} />
       </div>
     );
   }
--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import Car from './Cars';
 
 class Mycars extends Component{
+
+  noCopy = () => {
+    alert("merci de ne pas copier le texte");
+  }
+
+
+
   render(){
     return (
       <div>
         <h1>{this.props.title}</h1>
+        <p onCopy={this.noCopy}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         <Car color='red'>Ford</Car>
         <Car color='black'>Mercedes</Car>
         <Car></Car>
